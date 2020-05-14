00dd965963c87b4e4fcc5a82557ca350c12362ff
betovawn1 <sergioluizfrancajr@gmail.com>
2020-05-14 15:36:11 -0300

    header mobile e footer desktop/mobile

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const http = require('http')
 const express = require('express')
-const path = require('path')
 const app = express()
 
 app.use(express.static('dist'))
--- src/js/main-echeese.js
+++ src/js/main-echeese.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const $signIn = document.querySelector('.header-sup .sign-in')
+    const $signIn = document.querySelector('header .sing-in')
+    const $menuMobile = document.querySelector('#menu-mobile')
     vtexjs.checkout.getOrderForm()
         .done(function(orderForm) {
+            console.log('order form ==> ', orderForm)
             if (orderForm.loggedIn) {
-                $signIn.classList.add('logged')
-                if (orderForm.clientProfileData.firstName) {
-                    $signIn.querySelector('.welcome-message .welcome-name').innerText = orderForm.clientProfileData.firstName
-                } else if (orderForm.clientProfileData.email) {
-                    $signIn.querySelector('.welcome-message .welcome-name').innerText = orderForm.clientProfileData.email
+                if (window.innerWidth > 991) {
+                    $signIn.classList.add('logged')
+                    if (orderForm.clientProfileData.firstName) {
+                        $signIn.querySelector('.welcome-signed .welcome-name').innerText = orderForm.clientProfileData.firstName
+                    } else if (orderForm.clientProfileData.email) {
+                        $signIn.querySelector('.welcome-signed .welcome-name').innerText = orderForm.clientProfileData.email
+                    }
+                } else {
+                    $menuMobile.classList.add('logged')
+                    if (orderForm.clientProfileData.firstName) {
+                        $menuMobile.querySelector('.menu-mobile-account .welcome-signed-mobile .welcome-name').innerText = orderForm.clientProfileData.firstName
+                    } else if (orderForm.clientProfileData.email) {
+                        $menuMobile.querySelector('.menu-mobile-account .welcome-signed-mobile .welcome-name').innerText = orderForm.clientProfileData.email
+                    }
                 }
             }
         })
     document.querySelector('.hamburguer-menu').addEventListener('click', function(ev) {
-        document.getElementById('menu-mobile').classList.toggle('d-block')
+        document.getElementById('menu-mobile').classList.add('d-block')
+        document.querySelector('.overlay-menu-mobile').classList.add('d-block')
+    })
+    document.querySelector('.overlay-menu-mobile').addEventListener('click', function() {
+        document.getElementById('menu-mobile').classList.remove('d-block')
+        document.querySelector('.overlay-menu-mobile').classList.remove('d-block')
     })
     document.getElementById('menu-mobile')
         .querySelectorAll('.menu-list-mobile h3')
